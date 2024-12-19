2716e50702afb2edeb7a64222c335251210d7925
ilxplay <illiasuper46@gmail.com>
2024-12-19 20:46:19 +0100

    *LARGE UPDATE*
    Added some aceternity ui components.
    Added spotlight and theme providers.
    Added grids and dots background.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css"
+import { ThemeProvider } from "@/components/ThemeProvider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +23,14 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
 
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
 
       </body>
     </html>
